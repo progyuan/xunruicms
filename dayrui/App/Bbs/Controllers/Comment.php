1320cfe16101ed1e8a20879e54bc1d43701af9fd
迅睿云软件 <dayrui@gmail.com>
2019-07-29 05:20:13 +0000

    no commit message

---
+++ dayrui/App/Bbs/Controllers/Comment.php
@@ -0,0 +1,28 @@
+<?php namespace Phpcmf\Controllers;
+
+/**
+ * 二次开发时可以修改本文件，不影响升级覆盖
+ */
+
+class Comment extends \Phpcmf\Home\Comment
+{
+
+    public function index() {
+        parent::_Index();
+    }
+
+    public function post() {
+        parent::_Post();
+    }
+
+
+    public function op() {
+        parent::_Op();
+    }
+
+    // 格式化评论内容，方便二次开发和重写
+    public function _safe_replace($data) {
+
+        return str_replace(['<p><br/></p>'], '', $_POST['data']['content']);
+    }
+}
---
+++ dayrui/Fcms/Readme.txt
@@ -0,0 +1,2 @@
+此目录属于PHPCMF核心程序目录，禁止修改文件内容，会引起系统的不稳定
+二次开发注意：http://help.phpcmf.net/491.html
